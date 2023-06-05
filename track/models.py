0158c2913c1125fecf26cec353d8923ca860c268
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-06-05 19:33:16 +0000

    Created Remark model

--- track/models.py
+++ track/models.py
@@ -5,11 +5,12 @@ from django.conf import settings
 
 
 class Member(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     bio = models.TextField(max_length=500, null=True, blank=True)
 
 
-class Activities(models.Model):
+class Activity(models.Model):
 
     PENDING_STATE = 'P'
     DONE_STATE = 'D'
@@ -20,7 +21,12 @@ class Activities(models.Model):
     ]
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
-    status = models.CharField( max_length=1, choices=ACTIVITY_STATE, default=PENDING_STATE)
+    status = models.CharField(max_length=1, choices=ACTIVITY_STATE, default=PENDING_STATE)
     poster = models.ForeignKey(Member, on_delete=models.PROTECT, related_name='activities')
-    remarks = models.TextField(max_length=300, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    
+class Remarks(models.Model):
+    commenter = models.ForeignKey(Member, on_delete=models.PROTECT,related_name='remarks')
+    activity = models.ForeignKey(Activity, on_delete=models.PROTECT, related_name='remarks')
+    comment = models.TextField(max_length=300, null=True, blank=True)
+    
