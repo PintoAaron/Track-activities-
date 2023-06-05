8bd377fcc06d7307ecbf1e135549ca5d9f62af21
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-06-05 21:09:34 +0000

    created Admin page

--- track/admin.py
+++ track/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Member,Activity,Remark
 
-# Register your models here.
+@admin.register(Member)
+class MemeberAdmin(admin.ModelAdmin):
+    list_display = [ 'id' ,'user','first_name','last_name', 'bio']
+    list_select_related = ['user']
+    
+    
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ['id','title','description','status','poster','timestamp']
+    list_select_related = ['poster']
+    
+    
+    
+@admin.register(Remark)
+class RemarkAdmin(admin.ModelAdmin):
+    list_display = ['id','comment','activity','commentor','date_created']
+    list_select_related = ['activity','commentor']
